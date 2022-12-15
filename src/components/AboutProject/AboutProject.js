2af20063e8614ec1c2a97722756f90389812c166
Ales-fox <Alex-170395@mail.ru>
2022-12-15 22:46:58 +0300

    Скорректирована основная странциа под 320px

--- src/components/AboutProject/AboutProject.js
+++ src/components/AboutProject/AboutProject.js
@@ -8,14 +8,14 @@ function AboutProject(props) {
             <div className="table_text table">
                 <h3 className="table__header">Дипломный проект включал 5 этапов</h3>
                 <h3 className="table__header">На выполнение диплома ушло 5 недель</h3>
-                <p className="table__text">Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.</p>
+                <p className="table__text table__text_1">Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.</p>
                 <p className="table__text">У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.</p>
             </div>
             <div className="table_rect table">
                <p className="table-cell_rect table-cell_rect_blue">1 неделя</p>
                <p className="table-cell_rect table-cell_rect_gray">4 недели</p>
-               <p className="table-cell">Backend</p>
-               <p className="table-cell">Frontend</p>
+               <p className="table-cell">Back-end</p>
+               <p className="table-cell">Front-end</p>
             </div>            
         </section>
     )
