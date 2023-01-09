97b0357ac42d798d7c3a35471bca935ad2fa845c
Ales-fox <Alex-170395@mail.ru>
2023-01-09 23:16:07 +0300

    Реализована кнопка Назад В NotFound

--- src/components/NotFoundPage/NotFoundPage.js
+++ src/components/NotFoundPage/NotFoundPage.js
@@ -1,15 +1,13 @@
-import { Link, useLocation } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import './NotFoundPage.css'
 
 function NotFoundPage(props) {
-    const location = useLocation();
-    console.dir(location);
-
+    
     return (
         <section className='notFound'>
             <p className="notFound__number">404</p>
             <p className="notFound__text">Страница не найдена</p>
-            <Link className='link notFound__link' to={{from: location}}>Назад</Link>            
+            <NavLink className='link notFound__link' to={-2}>Назад</NavLink>            
         </section>
     )
 }
